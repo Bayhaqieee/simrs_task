48c4c14b8be67d205723cf0009fba7f340bd0506
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-03-01 22:11:37 +0700

    Session Commit

---
+++ pasien.java
@@ -0,0 +1,17 @@
+class Pasien {
+    int id;
+    String nama;
+    int umur;
+    String alamat;
+    
+    public Pasien(int id, String nama, int umur, String alamat) {
+        this.id = id;
+        this.nama = nama;
+        this.umur = umur;
+        this.alamat = alamat;
+    }
+    
+    public String toString() {
+        return "ID: " + id + ", Nama: " + nama + ", Umur: " + umur + ", Alamat: " + alamat;
+    }
+}
